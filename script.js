45cc4d82a51c08e1a89c88f6dc92a8cf2aa63031
Mayank singh chauhan <113712051+MAYANKSINGHCHAUHAN@users.noreply.github.com>
2025-04-07 13:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,29 +2,29 @@
 function convertToRoman(num) {
     // Array of Roman numeral symbols and their corresponding decimal values
     const romanSymbols = [
-        ['M', 1000], 
-        ['CM', 900], 
-        ['D', 500], 
-        ['CD', 400], 
-        ['C', 100], 
-        ['XC', 90], 
-        ['L', 50], 
-        ['XL', 40], 
-        ['X', 10], 
-        ['IX', 9], 
-        ['V', 5], 
-        ['IV', 4], 
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
         ['I', 1]
     ];
 
     let result = ''; // Initialize the result string
 
     // Loop through each Roman numeral symbol starting from the largest
-    for (let i = 0; i < romanSymbols.length; i++) {
-        // While the number is larger than or equal to the Roman numeral value
-        while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0]; // Append the Roman symbol
-            num -= romanSymbols[i][1]; // Subtract the corresponding value
+    for (let [symbol, value] of romanSymbols) {
+        // While the number is greater than or equal to the current Roman numeral value
+        while (num >= value) {
+            result += symbol; // Append the Roman symbol to the result
+            num -= value; // Subtract the corresponding value from num
         }
     }
 
@@ -36,6 +36,7 @@ function convert() {
     const inputNumber = document.getElementById('inputNumber').value; // Get the input value
     const resultDiv = document.getElementById('result'); // Get the result div
 
+    // Validate the input
     if (inputNumber === '' || inputNumber < 0 || inputNumber > 100000) {
         resultDiv.textContent = 'Please enter a valid number between 0 and 100000';
     } else {
